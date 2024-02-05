5924d96960c8b547f2129a006ca26bb19eeb1d0c
Bsoulmindy <oussamabader@gmail.com>
2024-02-05 20:39:21 +0100

    Configuring CMake #2

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <QQmlContext>
 #include <QtQml/QQmlExtensionPlugin>
 #include "../config/config.h"
+#include <QFontDatabase>
+#include <QIcon>
 
 int main(int argc, char *argv[])
 {
@@ -18,6 +20,17 @@ int main(int argc, char *argv[])
         []() { QCoreApplication::exit(-1); },
         Qt::QueuedConnection);
     Q_IMPORT_QML_PLUGIN(CustomComponentsPlugin)
+
+    int id = QFontDatabase::addApplicationFont(":/fonts/stratum2demium.ttf");
+    if(id == -1) {
+        qCritical() << "Couldn't load the font";
+    } else {
+        QString family_font = QFontDatabase::applicationFontFamilies(id).at(0);
+        QFont stratum_font(family_font);
+        app.setFont(stratum_font);
+    }
+    app.setWindowIcon(QIcon(":/icons/revision_assistant_icon.png"));
+
     engine.rootContext()->setContextProperty("appVersion", PROJECT_VERSION);
     engine.loadFromModule("RevisionAssistant", "Main");
 
