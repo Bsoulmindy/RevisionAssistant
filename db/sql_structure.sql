eb9a7e181db89a461a2e224a5648d7c2e7e3a1ba
Bsoulmindy <oussamabader@gmail.com>
2024-02-08 19:54:12 +0100

    Setting up CI/CD for Windows (1)

--- db/sql_structure.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE dict (
-    id INTEGER PRIMARY KEY,
-    question TEXT NOT NULL,
-    response TEXT NOT NULL,
-    isCheckedQuestion BOOLEAN NOT NULL DEFAULT 0,
-    isCheckedResponse BOOLEAN NOT NULL DEFAULT 0
-);
